ef7da6450c68344ddcfe7b8e967882ea1bf66280
devn0n <osoraanwankwo@gmail.com>
2025-02-03 22:29:35 +0100

    Implement nomba struct

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type nomba struct {
+	Number     string   `json:"number"`
+	Prime      bool     `json:"is_prime"`
+	Perfect    bool     `json:"is_perfect"`
+	Properties []string `json:"properties"`
+	Sum        int      `json:"price"`
+	Fact       string   `json:"fun_fact"`
+}
