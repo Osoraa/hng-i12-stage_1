22480d43b562dd1d521c3b2d83b6756e8b0a8730
devn0n <osoraanwankwo@gmail.com>
2025-02-03 23:57:01 +0100

    Implement main - handle cors - Implement router

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type nomba struct {
-	Number     string   `json:"number"`
+	Number     int      `json:"number"`
 	Prime      bool     `json:"is_prime"`
 	Perfect    bool     `json:"is_perfect"`
 	Properties []string `json:"properties"`
-	Sum        int      `json:"price"`
+	Sum        int      `json:"digit_sum"`
 	Fact       string   `json:"fun_fact"`
+}
+
+// main function
+func main() {
+	// Create router
+	router := gin.Default()
+
+	// Handle CORS
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+
+	// GET request
+	router.GET("/api/classify-number/:num", getNumber)
+
+	// Run server
+	router.Use(cors.New(config))
+	router.Run("localhost:8080")
 }
